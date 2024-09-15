39d92fb8a44df02fe4772dfc69f373a6db71055a
TLI <tli@thelastimperial.com>
2024-09-14 19:29:38 -0700

    Added patient index to search all patient with pagination.

--- patient/src/main/java/com/tli/patient/domain/repositories/PatientRepository.java
+++ patient/src/main/java/com/tli/patient/domain/repositories/PatientRepository.java
@@ -1,9 +1,15 @@
 package com.tli.patient.domain.repositories;
 
 import com.tli.patient.domain.entities.PatientEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PatientRepository extends CrudRepository<PatientEntity, Long> {
+public interface PatientRepository extends CrudRepository<PatientEntity, Long>,
+        PagingAndSortingRepository<PatientEntity, Long> {
+    Page<PatientEntity> findByNameIgnoreCaseContainingOrFatherLastNameIgnoreCaseContainingOrMotherLastNameIgnoreCaseContaining(
+            String name, String fatherLastName, String motherLastName, Pageable page);
 }
